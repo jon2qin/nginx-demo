5ebcccac9b16193b273fdfd81aa4c19180ad16e4
jonqin <jon2qin@163.com>
2025-09-28 11:35:22 +0800

    enhance the test api

--- src/main/java/org/example/nginxdemo/controller/HelloWoldController.java
+++ src/main/java/org/example/nginxdemo/controller/HelloWoldController.java
@@ -1,30 +1,20 @@
 package org.example.nginxdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Random;
-
 @RestController
-@RequestMapping(value = "/test")
+@RequestMapping(value = "/api")
 public class HelloWoldController {
 
     @GetMapping(value = "/hello")
-    public String hello() {
-        Random random = new Random();
-        int nextInt = random.nextInt(101);
-        HashMap<Object, Object> map = new HashMap<>(nextInt);
-        for (int i = 0; i < nextInt; i++) {
-            map.put(i, new Object());
-        }
-        return "Hello Wold! size = " + map.size();
-    }
-
-    @GetMapping(value = "/good")
-    public String test() {
-        return "soga!";
+    public String hello(HttpServletRequest request) {
+        String remoteAddr = request.getRemoteAddr();
+        return "Hello Wold! " +
+                "version = v1.0.0 " +
+                "remoteAddr = " + remoteAddr;
     }
 
 }
